b530b28446502943c851179de989e736081550ae
mmh <7dea508e25ba4b8b78c2af21bbd1460fc5761c9e>
2019-04-01 18:50:57 +0200

    //

--- app/src/main/java/com/example/mido/medicalemergency/HomeActivity.java
+++ app/src/main/java/com/example/mido/medicalemergency/HomeActivity.java
@@ -1,17 +1,38 @@
 package com.example.mido.medicalemergency;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 import com.example.mido.medicalemergency.Doctors.DoctorsActivity;
 import com.example.mido.medicalemergency.Slider.CPRActivity;
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.ResultCallbacks;
+import com.google.android.gms.common.api.Status;
+
+import java.net.URI;
+import java.net.URL;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
+
+    private GoogleApiClient googleApiClient;
+
+    private String name ="", mail="" ;
+    private Uri imguri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +42,59 @@ public class HomeActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         ButterKnife.bind(this);
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
 
+        googleApiClient=new GoogleApiClient.Builder(this)
+                .enableAutoManage(this, this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API ,  gso)
+                .build();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if(opr.isDone()){
+            GoogleSignInResult result=opr.get();
+            handleSignInResult(result);
+        }else {
+            goLogInActivity();
+        }
+    }
+
+    void LogOutFunction(){
+        Auth.GoogleSignInApi.signOut(googleApiClient).setResultCallback(new ResultCallback<Status>() {
+            @Override
+            public void onResult(@NonNull Status status) {
+                if(status.isSuccess()){
+                    goLogInActivity();
+                }else{
+                    Toast.makeText(getApplicationContext() , "error while Log out" , Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void goLogInActivity() {
+        Intent intent = new Intent(this , LoginActivity.class );
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    private void setdata(String displayName, String email, Uri s) {
+        name=displayName;
+        mail=email;
+        imguri=s;
+
+        Toast.makeText(this, name , Toast.LENGTH_SHORT).show();
+    }
+
+    private void handleSignInResult(GoogleSignInResult result) {
+        GoogleSignInAccount account = result.getSignInAccount();
+        setdata(account.getDisplayName() , account.getEmail(),account.getPhotoUrl());
     }
 
     @OnClick(R.id.hospitalImageView)
@@ -57,9 +130,13 @@ public class HomeActivity extends AppCompatActivity {
 
     @OnClick(R.id.medicineImageView)
     void onMedicineClicked() {
+        // until handel ui for log out
+        LogOutFunction();
     }
 
 
-
-
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this , connectionResult.getErrorMessage() ,Toast.LENGTH_LONG ).show();
+    }
 }
--- app/src/main/java/com/example/mido/medicalemergency/LoginActivity.java
+++ app/src/main/java/com/example/mido/medicalemergency/LoginActivity.java
@@ -1,16 +1,31 @@
 package com.example.mido.medicalemergency;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApi;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+
+import java.net.Authenticator;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
 
     @BindView(R.id.login_but)
@@ -25,8 +40,9 @@ public class LoginActivity extends AppCompatActivity {
     @BindView(R.id.password)
     TextInputEditText passwordInput;
 
-    @BindView(R.id.signup)
-    TextView signUp;
+    private GoogleApiClient googleApiClient;
+
+    private static final int GOOGLE_REQWEST = 785;
 
 
     @Override
@@ -34,5 +50,63 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+
+
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        googleApiClient=new GoogleApiClient.Builder(this)
+                .enableAutoManage(this,this)
+                .addApi(Auth.GOOGLE_SIGN_IN_API ,  gso)
+                .build();
+
+        logInGoogle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = Auth.GoogleSignInApi.getSignInIntent(googleApiClient);
+                startActivityForResult(intent,GOOGLE_REQWEST);
+            }
+        });
+
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Toast.makeText(this , connectionResult.getErrorMessage() ,Toast.LENGTH_LONG ).show();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GOOGLE_REQWEST){
+            GoogleSignInResult result=Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            handelSignInResult(result);
+        }
+    }
+
+    private void handelSignInResult(GoogleSignInResult result) {
+        if (result.isSuccess()){
+            openhome();
+        }else{
+            Toast.makeText(this,"error while login try agan" , Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void openhome() {
+        Intent intent = new Intent(this , HomeActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        OptionalPendingResult<GoogleSignInResult> opr = Auth.GoogleSignInApi.silentSignIn(googleApiClient);
+        if(opr.isDone()){
+            openhome();
+        }
     }
 }
